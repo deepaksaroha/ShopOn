6ae6708ebc0059f8255d2ead968ccd51fd5478ae
deepaksaroha <deepaksaroha1993@gmail.com>
2021-12-01 10:32:28 +0530

    added routes for products and categories apis

--- server/api.js
+++ server/api.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const users = require('./routes/users');
 const sessions = require('./routes/sessions');
+const products = require('./routes/products');
+const categories = require('./routes/categories')
 
 // Add json and urlencoded middleware
 router.use(express.json());
@@ -11,5 +13,9 @@ router.use(express.urlencoded({ extended: true }));
 router.use('/users', users);
 
 router.use('/sessions', sessions);
+
+router.use('/products', products);
+
+router.use('/categories', categories);
 
 module.exports = router;
