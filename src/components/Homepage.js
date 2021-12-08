96e26be00b1ec6117fb90aafccbcb629b3cf2f5d
deepaksaroha <deepaksaroha1993@gmail.com>
2021-12-09 01:11:11 +0530

    created home component

---
+++ src/components/Homepage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Categories from '../components/Categories'
+import Recommndations from '../components/Recommndations'
+import Banners from '../components/Banners'
+
+class Homepage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+
+    render(){
+        return(
+            <React.Fragment>
+                <Navbar />
+                <Categories />
+                <Banners />
+                <Recommndations />
+            </React.Fragment>
+        )
+    }
+}
+
+export default Homepage;
