d1600541ed716988db4c0f56ef191dc3759b490c
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-10 23:53:03 +0530

    created basic product description component

--- src/components/Productdescription.js
+++ src/components/Productdescription.js
@@ -7,6 +7,9 @@ class ProductDescription extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+            isLoaded: false,
+            isLoggedIn: false,
+            cartCount: 0,
             productData: {},
             cart: [],
             quantity: 0
@@ -14,20 +17,42 @@ class ProductDescription extends React.Component{
     }
 
     getProductData(){
-        axios.get('https://localhot:4000/api/products/'+this.props.match.params.productId)
-        .then(response=>response.json())
+        axios.get('/api/products/'+this.props.match.params.productId)
         .then(response=>{
             this.setState({
-                productData: response.productData
+                productData: response.data.productData
             })
+        })
+        .catch(error=>{
+            console.log('Something went wrong')
         })
     }
 
-    getCartData(){
-        axios.get('https://localhost:4000/api/users/cart')
+    getCartData=()=>{
+        axios.get('/api/users/cart')
         .then(response=>{
             this.setState({
-                cart: response.cart
+                cart: response.data.cart,
+                cartCount: response.data.cart.length
+            })
+        })
+        .catch(error=>{
+            console.log('Something went wrong')
+        })
+    }
+
+    getLoginStatus=()=>{
+        axios.get('/api/sessions')
+        .then(response=>{
+            this.setState({
+                isLoggedIn: true,
+                isLoaded: true
+            })
+        })
+        .catch(_=>{
+            this.setState({
+                isLoggedIn: false,
+                isLoaded: true
             })
         })
     }
@@ -35,43 +60,52 @@ class ProductDescription extends React.Component{
     componentDidMount(){
         this.getProductData();
         this.getCartData();
+        this.getLoginStatus();
     }
 
-    handleChange(e){
+    handleLogout=()=>{
+        axios.delete('/api/sessions')
+        .then(response=>{
+            this.getCartData();
+            this.getLoginStatus();
+        })
+        .catch(_=>{
+            this.getCartData();
+            this.getLoginStatus();
+        })
+    }
+
+    handleChange=(e)=>{
         this.setState({
             [e.target.name]: e.target.value
         })
     }
 
 
-    handleAddToCart(){
-        axios.put('https://localhot:4000/api/users/cart',
+    handleAddToCart=()=>{
+        axios.put('/api/users/cart',
             {
-                data: {
-                    productId: this.state.productData.productId,
-                    name: this.state.productData.name,
-                    quantity: this.state.quantity,
-                    price: this.state.productData.price
-                }
+                productId: this.state.productData.productId,
+                name: this.state.productData.name,
+                quantity: this.state.quantity,
+                price: this.state.productData.price
             }
         )
         .then(response=>{
-            this.props.handleAddProduct();
+            this.getCartData();
         })
         .catch(error=>{
             console.log(error.error);
         })
     }
 
-    handleBuy(){
-        axios.put('https://localhot:4000/api/users/cart',
+    handleBuy=()=>{
+        axios.put('/api/users/cart',
             {
-                data: {
-                    productId: this.state.productData.productId,
-                    name: this.state.productData.name,
-                    quantity: this.state.quantity,
-                    price: this.state.productData.price
-                }
+                productId: this.state.productData.productId,
+                name: this.state.productData.name,
+                quantity: this.state.quantity,
+                price: this.state.productData.price
             }
         )
         .then(response=>{
@@ -86,35 +120,38 @@ class ProductDescription extends React.Component{
 
         const cartProductIds = this.state.cart.map(product=>product.productId)
         const productData = this.state.productData;
-
-        return(
-            <React.Fragment>
-                <Navbar cartCount={this.state.cartCount} />
-                <img src="" alt="" />
-                <p>{productData.name}</p>
-                <p>{productData.price}</p>
-                <label>Units:</label>
-                <select name="quantity" onChange={this.handleChange} value={this.state.quantity}>
-                    <option for="quantity" value="1">1</option>
-                    <option for="quantity" value="2">2</option>
-                    <option for="quantity" value="3">3</option>
-                    <option for="quantity" value="4">4</option>
-                    <option for="quantity" value="5">5</option>
-                </select>
-                {
-                    productData.productId in cartProductIds?
-                    <button><Link to="/cart">Add to Cart</Link></button>
-                    :
-                    <button onClick={this.handleAddToCart}>Add to Cart</button>
-                }
-                {
-                    productData.productId in cartProductIds?
-                    <button><Link to="/cart">Buy Now</Link></button>
-                    :
-                    <button onClick={this.handleBuy}>Buy Now</button>
-                }
-            </React.Fragment>
-        )
+        if(this.state.isLoaded){
+            return(
+                <React.Fragment>
+                    <Navbar loginStatus={this.state.isLoggedIn} handleLogout={this.handleLogout} cartCount={this.state.cartCount}/>
+                    <img src="" alt="" />
+                    <p>{productData.name}</p>
+                    <p>{productData.price}</p>
+                    <label>Units:</label>
+                    <select name="quantity" onChange={this.handleChange} value={this.state.quantity}>
+                        <option htmlFor="quantity" value="1">1</option>
+                        <option htmlFor="quantity" value="2">2</option>
+                        <option htmlFor="quantity" value="3">3</option>
+                        <option htmlFor="quantity" value="4">4</option>
+                        <option htmlFor="quantity" value="5">5</option>
+                    </select>
+                    {
+                        cartProductIds.includes(productData.productId) ?
+                        <button><Link to="/cart">Add to Cart</Link></button>
+                        :
+                        <button onClick={this.handleAddToCart}>Add to Cart</button>
+                    }
+                    {
+                        cartProductIds.includes(productData.productId) ?
+                        <button><Link to="/cart">Buy Now</Link></button>
+                        :
+                        <button onClick={this.handleBuy}>Buy Now</button>
+                    }
+                </React.Fragment>
+            )
+        }else{
+            return ''
+        }        
     }
 }
 
