e32765993056f30124a7ec0903fff931bc13b8d2
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-10 23:41:14 +0530

    deleted unused code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import LoginPage from './LoginPage';
-import Profile from './Profile';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppRouter from './components/Approuter'
+
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route exact path="/" component={LoginPage} />
-          <Route exact path="/profile" component={Profile} />
-        </Switch>
+        <AppRouter />
     </Router>
   );
 }
