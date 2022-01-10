0b7bc71c6372d324292038adf39ec8e2aad75c76
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-10 23:46:03 +0530

    created homepage component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,26 +1,83 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Categories from '../components/Categories'
-import Recommndations from '../components/Recommndations'
-import Banners from '../components/Banners'
+import Navbar from './Navbar'
+import Categories from './Categories'
+// import Recommendations from './Recommendations'
+// import Banners from './Banners'
+import axios from 'axios'
 
 class Homepage extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-
+            isLoaded: false,
+            isLoggedIn: false,
+            cartCount: 0,
+            error: ''
         }
     }
 
+    getLoginStatus=()=>{
+        axios.get('/api/sessions')
+        .then(response=>{
+            this.setState({
+                isLoggedIn: true,
+                isLoaded: true
+            })
+        })
+        .catch(_=>{
+            this.setState({
+                isLoggedIn: false,
+                isLoaded: true
+            })
+        })
+    }
+
+    getCartCount=()=>{
+        axios.get('/api/users/cart')
+        .then(response=>{
+            this.setState({
+                cartCount: response.data.cart.length
+            })
+        })
+        .catch(error=>{
+            console.log(error);
+            this.setState({
+                error: error
+            })
+        })
+    }
+
+    componentDidMount(){
+        this.getCartCount();
+        this.getLoginStatus();
+    }
+
+    handleLogout=()=>{
+        axios.delete('/api/sessions')
+        .then(response=>{
+            this.getCartCount();
+            this.getLoginStatus();
+        })
+        .catch(_=>{
+            this.getCartCount();
+            this.getLoginStatus();
+        })
+    }
+
+
     render(){
-        return(
-            <React.Fragment>
-                <Navbar />
-                <Categories />
-                {/* <Banners />
-                <Recommndations /> */}
-            </React.Fragment>
-        )
+        if(this.state.isLoaded){
+            return(
+                <React.Fragment>
+                    <Navbar loginStatus={this.state.isLoggedIn} handleLogout={this.handleLogout} cartCount={this.state.cartCount} />
+                    <Categories />
+                    {/* <Banners /> */}
+                    {/* <Recommendations /> */}
+                </React.Fragment>
+            )
+        }else{
+            return ''
+        }
     }
 }
 
