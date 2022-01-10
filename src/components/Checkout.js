78bf57904310a54a27069f5b2c427b3f63b9d494
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-10 23:48:02 +0530

    basic checkout component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,82 +1,118 @@
 import React from 'react'
 import axios from 'axios'
 import Checkoutitem from './Checkoutitem';
-import Navbar from '../components/Navbar'
+import Navbar from './Navbar'
+import paymentHandlers from './paymentHandlers';
+import onOrderCreateFailure from './OrderCreateFailure'
+import { initiatePayment } from './Payment'
 
 class Checkout extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            cart : [],
-            loginStatus: false
+            isLoaded: false,
+            isLoggedIn: false,
+            cartCount: 0,
+            cart : []
         }
     }
 
-    componentDidMount(){
-        axios.get('https://localhost:4000/api/sessions')
+    getCart=()=>{
+        axios.get('/api/users/cart')
         .then(response=>{
             this.setState({
-                loginStatus: true
-            })
-        })
-
-
-        axios.get('https://localhost:4000/api/users/cart')
-        .then(response=>{
-            this.setState({
-                cart: response.cart
+                cart: response.data.cart,
+                cartCount: response.data.cart.length
             })
         })
         .catch(error=>{
             console.log('Something went wrong')
         })
     }
+    
+    getLoginStatus=()=>{
+        axios.get('/api/sessions')
+        .then(response=>{
+            this.setState({
+                isLoggedIn: true,
+                isLoaded: true
+            })
+        })
+        .catch(_=>{
+            this.setState({
+                isLoggedIn: false,
+                isLoaded: true
+            })
+        })
+    }
 
-    toLogin(){
+    componentDidMount(){
+        this.getCart();
+        this.getLoginStatus();
+    }
+
+    handleLogout=()=>{
+        axios.delete('/api/sessions')
+        .then(response=>{
+            this.getCart();
+            this.getLoginStatus();
+        })
+        .catch(_=>{
+            this.getCart();
+            this.getLoginStatus();
+        })
+    }
+
+    toLogin=()=>{
         this.props.history.push('/login');
     }
 
-    handleBuy(){
-        
+    handleBuy=()=>{
+        initiatePayment(paymentHandlers, onOrderCreateFailure);
     }
 
     render(){
-        if(!this.state.login){
-            this.toLogin();
-        }
-
-        let amount = 0;
-        this.state.cart.forEach(cartItem=>{
-            amount += cartItem.quantity*cartItem.price;
+        let totalAmount = 0;
+        this.state.cart.forEach(item=>{
+            totalAmount += item.quantity*item.price
         })
 
-
-        return(
-            <React.Fragment>
-                <Navbar />
-                {
-                    this.state.cart.length>0?
-                    <div>
+        if(this.state.isLoaded){
+            if(!this.state.isLoggedIn){
+                this.toLogin();
+            }
+    
+    
+            return(
+                <React.Fragment>
+                    <Navbar loginStatus={this.state.isLoggedIn} handleLogout={this.handleLogout} cartCount={this.state.cartCount} />
+                    {
+                        this.state.cart.length>0?
                         <div>
-                            {
-                                this.state.cart.map(cartItem=>{
-                                    return <Checkoutitem item={cartItem} />
-                                })
-                            }
+                            <div>
+                                {
+                                    this.state.cart.map(cartItem=>{
+                                        return <Checkoutitem item={cartItem} />
+                                    })
+                                }
+                            </div>
+                            <div>
+                                <p>Cart Value: Rs {totalAmount}</p>
+                                <p>Delivery Charge: Rs 50</p>
+                                <p>Total Amount: Rs {totalAmount+50}</p>
+                            </div>
+                            <button onClick={this.handleBuy}>Buy</button>
                         </div>
-                        <div>
-                            <p>Cart Value: Rs {amount}</p>
-                            <p>Delivery Charge: Rs 50</p>
-                            <p>Total Amount: Rs {amount+50}</p>
-                        </div>
-                        <button onClick={this.handleBuy}>Buy</button>
-                    </div>
-                    :
-                    <p>Your Cart is Empty</p>
-                }
-                
-            </React.Fragment>
-        )
+                        :
+                        <p>Your Cart is Empty</p>
+                    }
+                    
+                </React.Fragment>
+            )
+        }
+        else{
+            return ''
+        }
     }
 }
 
--- src/components/Checkoutitem.js
+++ src/components/Checkoutitem.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React from 'react';
 
 class Checkoutitem extends React.Component {
