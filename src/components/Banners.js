5a3e6301fe325beb7a309b7ea3eafea4b84254a7
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-12 14:15:36 +0530

    added banner component

---
+++ src/components/Banners.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import getBannerImgURLs from './getBannerImgURLs';
+
+function Banner(props){
+    const [count, setCount] = useState(0);
+
+    const banners = getBannerImgURLs();
+
+    function changeImageP(){
+        setCount(prevCount=>{
+            if(prevCount+1<0){
+                return banners.length - (prevCount+1)%banners.length;
+            }else{
+                return (prevCount+1)%banners.length;
+            }
+        });
+    }
+
+    function changeImageN(){
+        setCount(prevCount=>{
+            if(prevCount-1<0){
+                return banners.length + (prevCount-1)%banners.length;
+            }else{
+                return (prevCount-1)%banners.length;
+            }
+        });
+    }
+
+    return (
+        <React.Fragment>
+            <img src='../images/banner1.png' alt="img" style={{width: '100vw', height: '50vh'}}/>
+            <button onClick={changeImageN}>previous</button>
+            <button onClick={changeImageP}>next</button>
+        </React.Fragment>
+    )
+}
+
+export default Banner;
