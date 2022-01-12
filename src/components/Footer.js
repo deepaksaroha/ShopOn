b27f8f66d5f89df9b27a47749cc9fb58b5c360c3
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-12 15:20:27 +0530

    added basic footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer(){
+    return (
+        <React.Fragment>
+            <div style={{padding: '40px+1em'}}></div>
+            <div className="footer">
+                Developed by : Deepak, &nbsp;
+                {/* <span style={{color: 'red'}}>&#10084;</span> */}
+                <span>Connect over: deepaksaroha@yahoo.com</span>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Footer;
