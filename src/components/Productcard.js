c8bae9707c8b19b9e1ccff3d6e88e9d12e81581b
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-19 22:40:26 +0530

    added div element to enclose product details

--- src/components/Productcard.js
+++ src/components/Productcard.js
@@ -43,22 +43,24 @@ function ProductCard(props){
 
     return <div>
         <div className="product-card-box" id={props.productId}>
-            <img className="card-img" src="../images/banner1.png" alt=""/>
-            <p className="card-product-title"><Link to={'/products/'+props.productId}>{props.name}</Link></p>
-            <p className="card-price">&#8377; {props.price}</p>
-            <div className="card-btns">
-                {
-                    props.inCart?
-                    <button><Link to="/cart">Add to Cart</Link></button>
-                    :
-                    <button onClick={handleAddToCart}>Add to Cart</button>
-                }
-                {
-                    props.inCart?
-                    <button><Link to="/cart">Buy Now</Link></button>
-                    :
-                    <button onClick={handleBuy}>Buy Now</button>
-                }
+            <img className="card-img" src="../images/banner2.png" alt=""/>
+            <div className="card-details-box">
+                <p className="card-product-title"><Link to={'/products/'+props.productId}>{props.name}</Link></p>
+                <p className="card-price">&#8377; {props.price}</p>
+                <div className="card-btns">
+                    {
+                        props.inCart?
+                        <button><Link to="/cart">Add to Cart</Link></button>
+                        :
+                        <button onClick={handleAddToCart}>Add to Cart</button>
+                    }
+                    {
+                        props.inCart?
+                        <button><Link to="/cart">Buy Now</Link></button>
+                        :
+                        <button onClick={handleBuy}>Buy Now</button>
+                    }
+                </div>
             </div>
         </div>
     </div>
