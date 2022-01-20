ff385d5d450035ad9fe6a07dbb2d2607ee6f2a26
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-20 23:07:39 +0530

    moved high to low sort option to new line

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../css/Sort.css'
 
 function Sort(props){
 
@@ -9,8 +10,9 @@ function Sort(props){
     return(
         <React.Fragment>
             <div className="sort-options-box">
+                <h5>SORT</h5>
                 <input id="low-to-high" type="radio" name="sort_option" value="true" onChange={handleChangeSort}/>
-                <label htmlFor="low-to-high">Low to High</label>
+                <label htmlFor="low-to-high">Low to High</label><br/>
                 <input id="high-to-low" type="radio" name="sort_option" value="false" onChange={handleChangeSort}/>
                 <label htmlFor="high-to-low">High to Low</label>
             </div>
