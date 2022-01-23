2d40882746254798fafba3a5cb6cedeec588d56a
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-23 18:03:52 +0530

    added deals section

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,6 +4,7 @@ import Categories from './Categories'
 // import Recommendations from './Recommendations'
 import Banners from './Banners'
 import axios from 'axios'
+import '../css/Homepage.css'
 
 class Homepage extends React.Component{
     constructor(props){
@@ -74,7 +75,25 @@ class Homepage extends React.Component{
                     <Navbar loginStatus={this.state.isLoggedIn} handleLogout={this.handleLogout} cartCount={this.state.cartCount} />
                     <Categories />
                     <Banners />
-                    {/* <Recommendations /> */}
+                    <div className="deals-outer-box">
+                        <h3>DEALS OF THE DAY</h3>
+                        <div>
+                            <h4>Phones and Gadgets Deals</h4>
+                            <div className="deals-box">
+                                <a href="/products/1"><div className="deal-img-box"><img src="../images/ID1_1.jpg" alt="" /></div></a>
+                                <a href="/products/2"><div className="deal-img-box"><img src="../images/ID2_1.jpg" alt="" /></div></a>
+                                <a href="/products/3"><div className="deal-img-box"><img src="../images/ID3_1.jpg" alt="" /></div></a>
+                            </div>
+                        </div>
+                        <div>
+                            <h4>Fashion and Accesories</h4>
+                            <div className="deals-box">
+                                <a href="/products/6"><div className="deal-img-box"><img src="../images/ID6_1.jpg" alt="" /></div></a>
+                                <a href="/products/7"><div className="deal-img-box"><img src="../images/ID7_1.jpg" alt="" /></div></a>
+                                <a href="/products/8"><div className="deal-img-box"><img src="../images/ID8_1.jpg" alt="" /></div></a>
+                            </div>
+                        </div>
+                    </div>
                 </React.Fragment>
             )
         }else{
