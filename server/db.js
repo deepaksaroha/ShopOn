47a4246930377ebcf017a403933a1cc9301c4282
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-29 12:28:22 +0530

    bug fix db configuration

--- server/db.js
+++ server/db.js
@@ -7,30 +7,30 @@ mongoose.set('useCreateIndex', true);
 exports.connect = (dbConfig) => {
     try {
 
-        // const {host, username, password, database} = dbConfig;
+        const {host, username, password, database} = dbConfig;
         // const {host, database} = dbConfig;
         // console.log(password);
-        // let absoluteHostName = host;
+        let absoluteHostName = host;
         
         // const mongoUri = `mongodb://${absoluteHostName}/${database}`;
         
-        // if (username) {
-        //     if (password !== undefined) {
-        //         console.log('in password')
-        //         absoluteHostName = `${username}:${password}@${absoluteHostName}`;
-        //     } else {
-        //         absoluteHostName = `${username}@${absoluteHostName}`;
-        //     }
-        // }
+        if (username) {
+            if (password !== undefined) {
+                console.log('in password')
+                absoluteHostName = `${username}:${password}@${absoluteHostName}`;
+            } else {
+                absoluteHostName = `${username}@${absoluteHostName}`;
+            }
+        }
 
         // const abc = `mongodb+srv://${absoluteHostName}/${database}?retryWrites=true&w=majority`;
 
         // console.log(abc);
 
-        // const mongoUri = `mongodb+srv://${absoluteHostName}/${database}?retryWrites=true&w=majority`;
+        const mongoUri = `mongodb+srv://${absoluteHostName}/${database}?retryWrites=true&w=majority`;
         
         
-        const mongoUri = `mongodb://localhost/test2`;
+        // const mongoUri = `mongodb://localhost/test2`;
         return mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
     } catch (err) {
         console.log(`Error connecting to MongoDB: ${err}`);
