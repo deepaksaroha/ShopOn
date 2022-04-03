6d86c34a4cc62dcdfe648cb75f574c7ec63e8c34
deepaksaroha <deepaksaroha1993@gmail.com>
2022-04-03 10:04:23 +0530

    refactored code

--- server/routes/carts.js
+++ server/routes/carts.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
 
-
 //get cart
 router.get('/', (req, res, next)=>{
     if(req.session.userId === undefined){
