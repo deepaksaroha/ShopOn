344ae7cc0090e169177cd412ed113634468a014d
deepaksaroha <deepaksaroha1993@gmail.com>
2022-06-10 18:09:42 +0530

    fixed small image position alignment

--- src/components/Checkoutitem.js
+++ src/components/Checkoutitem.js
@@ -14,7 +14,7 @@ class Checkoutitem extends React.Component {
 
         return (
             <React.Fragment>
-                <td><img className="chkout-img" src={`../images/ID${item.productId}_1.jpg`} alt="" /></td>
+                <td><div className="chkout"><img className="chkout-img" src={`../images/ID${item.productId}_1.jpg`} alt="" /></div></td>
                 <td><p>{item.name}</p></td>
                 <td><p>{item.quantity}</p></td>
                 <td><p>&#8377;{item.price *item.quantity}</p></td>
